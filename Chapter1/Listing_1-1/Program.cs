f2e7bf91aab4f08752e146b6421ef9b65fa24145
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-06 21:36:10 +0100

    Listing 1-1

---
+++ Chapter1/Listing_1-1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ThreadStart(ThreadMethod));
+            t.Start();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Main thread: Do some work.");
+                Thread.Sleep(0);
+            }
+            t.Join();
+        }
+
+        static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"ThreadProc: {i}");
+                Thread.Sleep(0);
+            }
+        }
+
+    }
+}
---
+++ Chapter1/Listing_1-1/README.md
@@ -0,0 +1,19 @@
+# Displays
+
+```bash
+> dotnet run
+ThreadProc: 0
+Main thread: Do some work.
+ThreadProc: 1
+Main thread: Do some work.
+ThreadProc: 2
+Main thread: Do some work.
+ThreadProc: 3
+Main thread: Do some work.
+ThreadProc: 4
+ThreadProc: 5
+ThreadProc: 6
+ThreadProc: 7
+ThreadProc: 8
+ThreadProc: 9
+```
