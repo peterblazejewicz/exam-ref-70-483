8873d3f5d24ce3dd656ce3ccb083c2b0a08321cc
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 11:29:45 +0100

    Thread pool

---
+++ Chapter1/Listing_1-7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxThreads;
+            int minThreads;
+            int completionPortThreads;
+            ThreadPool.GetMaxThreads(out maxThreads, out completionPortThreads);
+            ThreadPool.GetMinThreads(out minThreads, out completionPortThreads);
+            Console.WriteLine($"ThreadPool max threads: {maxThreads}");
+            Console.WriteLine($"ThreadPool min threads: {minThreads}");
+
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                Console.WriteLine("Working on a thread from threadpool");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
