61da235762d99fa3c70f13daa367127b828563e0
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 11:14:05 +0100

    Thread local data

---
+++ Chapter1/Listing_1-6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    class Program
+    {
+
+        static ThreadLocal<int> field = new ThreadLocal<int>(
+            () => Thread.CurrentThread.ManagedThreadId);
+
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                for (int i = 0; i < field.Value; i++)
+                {
+                    Console.WriteLine($"Thread A: {i}");
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < field.Value; i++)
+                {
+                    Console.WriteLine($"Thread B: {i}");
+                }
+            }).Start();
+
+            Console.ReadKey();
+        }
+    }
+}
