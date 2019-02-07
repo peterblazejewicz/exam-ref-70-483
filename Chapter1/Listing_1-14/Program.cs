a7f723b343fb2d4ff5d2b348daae7c4c778a9e08
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 16:29:01 +0100

    Waiting for tasks with WaitAll

---
+++ Chapter1/Listing_1-14/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            const int timeout = 1000;
+
+            List<Task> tasks = new List<Task>
+            {
+                Task.Run(() => {
+                    int index = ++i;
+                    Thread.Sleep(timeout);
+                    Console.WriteLine($"{index}");
+                    return index;
+                }),
+                Task.Run(() => {
+                    int index = ++i;
+                    Thread.Sleep(timeout);
+                    Console.WriteLine($"{index}");
+                    return index;
+                }),
+                Task.Run(() => {
+                    int index = ++i;
+                    Thread.Sleep(timeout);
+                    Console.WriteLine($"{index}");
+                    return index;
+                }),
+            };
+
+            Task.WaitAll(tasks.ToArray());
+        }
+    }
+}
