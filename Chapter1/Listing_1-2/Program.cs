c835d32f92a532534ebd885b5dad5ad3089e3dbd
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 09:10:47 +0100

    Threads 2

---
+++ Chapter1/Listing_1-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(ThreadMethod);
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        private static void ThreadMethod()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"ThreadProc: {i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
