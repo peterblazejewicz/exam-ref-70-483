fe6db5f5decfda258da8f491425b80dfb29fe144
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 14:35:29 +0100

    Task with continuation options

---
+++ Chapter1/Listing_1-11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = Task.Run(() => 42)
+                .ContinueWith(action => action.Result * 2);
+            task.ContinueWith(
+                action => Console.WriteLine("Cancelled"),
+                TaskContinuationOptions.OnlyOnCanceled)
+                .ContinueWith(
+                    action => Console.WriteLine("Faulted"),
+                    TaskContinuationOptions.OnlyOnFaulted);
+            Task completionTask = task.ContinueWith(
+                action => Console.WriteLine("Completed"),
+                TaskContinuationOptions.OnlyOnRanToCompletion
+            );
+            completionTask.Wait();
+        }
+    }
+}
