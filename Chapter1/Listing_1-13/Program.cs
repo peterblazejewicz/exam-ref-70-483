87b2a47331b0a67fd947e6faeb8fb5f5eb75475f
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 15:33:48 +0100

    Task factory with parent and child tasks

---
+++ Chapter1/Listing_1-13/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int[]> parentTask = Task.Run(() =>
+            {
+                int[] results = new int[3];
+
+                TaskFactory taskFactory = new TaskFactory(
+                    TaskCreationOptions.AttachedToParent,
+                    TaskContinuationOptions.ExecuteSynchronously
+                );
+                taskFactory.StartNew(() => results[0] = 0);
+                taskFactory.StartNew(() => results[1] = 1);
+                taskFactory.StartNew(() => results[2] = 2);
+
+                return results;
+            });
+
+            Task finalTask = parentTask.ContinueWith(parent =>
+            {
+                foreach (int i in parent.Result)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            });
+            finalTask.Wait();
+        }
+    }
+}
