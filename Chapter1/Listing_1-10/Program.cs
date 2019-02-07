4f99dbeab01283c43fc7d8bb21f365cd6e7ce523
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 14:20:35 +0100

    Task with continuation

---
+++ Chapter1/Listing_1-10/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> task = Task.Run(() => 42)
+            .ContinueWith(action => action.Result * 2);
+
+            Console.WriteLine($"Results: {task.Result}");
+        }
+    }
+}
