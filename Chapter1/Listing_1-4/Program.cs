b7d02580ed0c9564bb5d5a5a957de7c1bd8275be
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 09:40:15 +0100

    Stopping thread

---
+++ Chapter1/Listing_1-4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stopped = false;
+            Thread thread = new Thread(new ThreadStart(() =>
+            {
+                while (!stopped)
+                {
+                    Console.WriteLine("Running...");
+                    Thread.Sleep(1000);
+                }
+            }));
+            thread.Start();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            stopped = true;
+
+            thread.Join();
+        }
+
+    }
+}
