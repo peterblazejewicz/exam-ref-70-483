dc4a1926bcf475d9e12717475da63372bda6198b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 17:27:49 +0100

    Tasks run with WaitAny

---
+++ Chapter1/Listing_1-15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Task<int>[] tasks = new Task<int>[]{
+                Task.Run(() => {
+                    Thread.Sleep(2000);
+                    return 1;
+                }),
+                Task.Run(() => {
+                    Thread.Sleep(1000);
+                    return 2;
+                }),
+                Task.Run(() => {
+                    Thread.Sleep(3000);
+                    return 3;
+                }),
+            };
+
+            while (tasks.Length > 0)
+            {
+                int index = Task.WaitAny(tasks);
+                Task<int> completedTask = tasks[index];
+
+                Console.WriteLine(completedTask.Result);
+
+                List<Task<int>> temp = tasks.ToList();
+                temp.RemoveAt(index);
+
+                tasks = temp.ToArray();
+            }
+        }
+    }
+}
