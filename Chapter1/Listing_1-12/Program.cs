d52008f972d04703d890aba80af189e18874bcfd
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 14:47:54 +0100

    Parent task with child tasks

---
+++ Chapter1/Listing_1-12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int[]> parentTask = Task.Run(() =>
+            {
+                int[] results = new int[3];
+                new Task(() => results[0] = 0, TaskCreationOptions.AttachedToParent)
+                    .Start();
+                new Task(() => results[1] = 1, TaskCreationOptions.AttachedToParent)
+                    .Start();
+                new Task(() => results[2] = 2, TaskCreationOptions.AttachedToParent)
+                    .Start();
+                return results;
+            });
+
+            Task finalTask = parentTask.ContinueWith(action =>
+            {
+                foreach (int i in action.Result)
+                {
+                    Console.WriteLine(i);
+                }
+            });
+
+            finalTask.Wait();
+        }
+    }
+}
