3a0614ef2d167256dddd861c599563cc48ec7acd
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 18:29:22 +0100

    Async/Await introduction

---
+++ Chapter1/Listing_1-18/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            string results = await DownloadContent();
+            Console.WriteLine($"Results: {results}");
+        }
+
+        private static async Task<string> DownloadContent()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string results = await client.GetStringAsync("https://www.microsoft.com");
+                return results;
+            }
+        }
+    }
+}
