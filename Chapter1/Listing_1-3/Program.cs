6d7b732e44f79ed07db77009c6ddbad3164bca70
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 09:31:28 +0100

    Parametrized thread

---
+++ Chapter1/Listing_1-3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(ThreadMethod));
+            t.Start(5);
+            t.Join();
+        }
+
+        private static void ThreadMethod(object o)
+        {
+            for (int i = 0; i < (int)o; i++)
+            {
+                Console.WriteLine($"ThreadProc: {i}");
+                Thread.Sleep(0);
+            }
+        }
+    }
+}
