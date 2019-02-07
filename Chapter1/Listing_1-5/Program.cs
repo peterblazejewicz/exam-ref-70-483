92aa75032e557e33bcee9edbeddcc8b216355999
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 09:55:20 +0100

    ThreadStatic attribute

---
+++ Chapter1/Listing_1-5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1
+{
+    class Program
+    {
+
+        [ThreadStatic]
+        static int field;
+
+        static void Main(string[] args)
+        {
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    field++;
+                    Console.WriteLine($"Thread A: {field}");
+                }
+            }).Start();
+
+            new Thread(() =>
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    field++;
+                    Console.WriteLine($"Thread B: {field}");
+                }
+            }).Start();
+
+            Console.ReadKey();
+
+        }
+    }
+}
