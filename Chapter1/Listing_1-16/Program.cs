74d8ef12c02ac2de5a4c0c68b9b3644444740594
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 17:39:07 +0100

    Tasks run with Parrallel class

---
+++ Chapter1/Listing_1-16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parallel.For(0, 10, i =>
+            {
+                Console.WriteLine($"For: {i}");
+                Thread.Sleep(1000);
+            });
+
+            IEnumerable<int> numbers = Enumerable.Range(0, 10);
+            Parallel.ForEach(numbers, i =>
+            {
+                Console.WriteLine($"ForEach: {i}");
+                Thread.Sleep(1000);
+            });
+
+        }
+    }
+}
