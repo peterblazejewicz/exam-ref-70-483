cd9b44547e81d6f49fb0a0ab0f97a9548721253d
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 14:01:32 +0100

    Task with void results

---
+++ Chapter1/Listing_1-8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("*");
+                }
+            });
+
+            task.Wait();
+        }
+    }
+}
