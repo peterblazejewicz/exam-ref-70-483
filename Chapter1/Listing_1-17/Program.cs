61acd3ffeff5e689a32136f4ba291988d83b09c5
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-07 17:54:05 +0100

    Breaking Parallel tasks

---
+++ Chapter1/Listing_1-17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ParallelLoopResult results = Parallel.For(0, 1000, (int i, ParallelLoopState state) =>
+            {
+                if (i == 500)
+                {
+                    Console.WriteLine("Breaking loop");
+                    state.Break();
+                }
+                return;
+            });
+
+            Console.WriteLine($"results.IsCompleted: {results.IsCompleted}");
+            Console.WriteLine($"results.LowestBreakIteration: {results.LowestBreakIteration}");
+        }
+    }
+}
