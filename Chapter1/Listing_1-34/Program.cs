42c0ea368b64896aba8c4101d2497bcf885f7d76
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 11:35:05 +0100

    Using ConcurrentDictionary

---
+++ Chapter1/Listing_1-34/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentDictionary<string, int> dict = new ConcurrentDictionary<string, int>();
+            if (dict.TryAdd("k1", 42))
+            {
+                Console.WriteLine("Added");
+            }
+
+            if (dict.TryUpdate("k1", 21, 42))
+            {
+                Console.WriteLine("42 updated to 21");
+            }
+
+            // override uncoditionally
+            dict["k1"] = 42;
+
+            int r1 = dict.AddOrUpdate("k1", 3, (key, value) => value * 2);
+            int r2 = dict.GetOrAdd("k2", 3);
+
+            Console.WriteLine($"Value of k1: {r1}");
+            Console.WriteLine($"Value of k2: {r2}");
+        }
+    }
+}
