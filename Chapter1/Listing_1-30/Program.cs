783a4560684ac9a1170c3e96ceda8028da646d1a
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 11:00:30 +0100

    Using ConcurrentBag

---
+++ Chapter1/Listing_1-30/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            bag.Add(42);
+            bag.Add(21);
+
+            int results;
+            if (bag.TryTake(out results))
+            {
+                Console.WriteLine(results);
+            }
+            if (bag.TryPeek(out results))
+            {
+                Console.WriteLine($"There is a next item: {results}");
+            }
+        }
+    }
+}
