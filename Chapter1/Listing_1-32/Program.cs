0e6d4cce0abdc41817d9b2996e3cf13e5775d4b1
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 11:19:04 +0100

    Using ConcurrentStack

---
+++ Chapter1/Listing_1-32/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentStack<int> stack = new ConcurrentStack<int>();
+
+            stack.Push(42);
+
+            int results;
+            if (stack.TryPop(out results))
+            {
+                Console.WriteLine($"Popped: {results}");
+            }
+
+            stack.PushRange(new int[] { 1, 2, 3 });
+
+            int[] values = new int[2];
+            stack.TryPopRange(values);
+
+            foreach (int i in values)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
