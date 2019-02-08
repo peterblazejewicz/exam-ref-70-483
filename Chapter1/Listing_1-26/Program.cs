d3e6182a3a7be0039f3a3df5ddc7b9da008f3b00
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 09:49:55 +0100

    Using ForAll on results of AsParallel

---
+++ Chapter1/Listing_1-26/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numbers = Enumerable.Range(0, 20);
+
+            ParallelQuery<int> parallelResults = numbers
+                .AsParallel()
+                .Where(i => i % 2 == 0);
+
+            parallelResults.ForAll(i => Console.WriteLine(i));
+        }
+    }
+}
