3424dc4cf611002f7f120b2e4cce8d4b8ead3336
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 09:18:55 +0100

    LINQ parallelism

---
+++ Chapter1/Listing_1-22/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> numbers = Enumerable.Range(0, 100_000_000);
+            int[] parallelResults = numbers.AsParallel()
+                .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                .WithDegreeOfParallelism(4)
+                .Where(i => i % 2 == 0)
+                .ToArray();
+
+            Console.WriteLine(parallelResults.Length);
+        }
+    }
+}
