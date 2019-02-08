1bb7a2e530e81bc1c5257b6874ec07b4393e8f13
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 10:45:47 +0100

    BlockingCollection with concurrent tasks

---
+++ Chapter1/Listing_1-28/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockingCollection<string> collection = new BlockingCollection<string>();
+
+            Task readTask = Task.Run(() =>
+            {
+                while (true)
+                {
+                    string input = collection.Take();
+                    Console.WriteLine($"You've entered: \"{input}\"");
+                    Console.Write("enter text: ");
+                }
+            });
+
+            Task writeTask = Task.Run(() =>
+            {
+                Console.Write("enter text: ");
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(input)) break;
+                    collection.Add(input);
+                }
+            });
+
+            writeTask.Wait();
+        }
+    }
+}
