1f5d8cc45eda687954f9126219e97a26ee77316b
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 10:04:00 +0100

    Handling AggregateExceptions when using parallel processing

---
+++ Chapter1/Listing_1-27/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IEnumerable<int> numbers = Enumerable.Range(0, 20);
+                ParallelQuery<int> parallelResults = numbers
+                    .AsParallel()
+                    .Where(i => IsEven(i));
+                parallelResults.ForAll(i => Console.WriteLine(i));
+            }
+            catch (AggregateException ex)
+            {
+                Console.WriteLine($"There were {ex.InnerExceptions.Count} exceptions");
+            }
+        }
+
+        private static bool IsEven(int i)
+        {
+            if (i % 10 == 0)
+            {
+                throw new ArgumentException(nameof(i));
+            }
+            return i % 2 == 0;
+        }
+    }
+}
