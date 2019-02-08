c44012032fed30bf2074107535486db4c9b31812
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 11:11:00 +0100

    Consuming results from ConcurrentBag

---
+++ Chapter1/Listing_1-31/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+
+            Task.Run(() =>
+            {
+                bag.Add(42);
+                Thread.Sleep(1_000);
+                bag.Add(21);
+            });
+
+            Task.Run(() =>
+            {
+                Thread.Sleep(5_00);
+                foreach (int i in bag)
+                {
+                    Console.WriteLine(i);
+                }
+            }).Wait();
+        }
+    }
+}
