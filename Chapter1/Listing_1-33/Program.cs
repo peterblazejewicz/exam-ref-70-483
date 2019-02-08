f281295af101206d01fb78c99579503c04292fe2
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 11:22:56 +0100

    Using ConcurrentQueue

---
+++ Chapter1/Listing_1-33/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcurrentQueue<int> queue = new ConcurrentQueue<int>();
+            queue.Enqueue(42);
+
+            int results;
+            if (queue.TryDequeue(out results))
+            {
+                Console.WriteLine($"Dequeued: {results}");
+            }
+        }
+    }
+}
