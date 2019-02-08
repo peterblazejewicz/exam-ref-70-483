5c6aea994283c0d6a67729f8f3b2c9a66446f892
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 09:24:43 +0100

    LINQ parallel unsorted results

---
+++ Chapter1/Listing_1-23/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IEnumerable<int> numbers = Enumerable.Range(0, 10);
+            int[] parallelResults = numbers.AsParallel()
+                .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                .WithDegreeOfParallelism(4)
+                .Where(i => i % 2 == 0)
+                .ToArray();
+
+            foreach (int i in parallelResults)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
