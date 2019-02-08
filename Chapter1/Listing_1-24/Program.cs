ca5d5dd520526bd94eb1541b573d85301f1c1626
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 09:30:35 +0100

    LINQ ordered results when using AsParallel()

---
+++ Chapter1/Listing_1-24/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IEnumerable<int> numbers = Enumerable.Range(0, 10);
+            int[] parallelResults = numbers.AsParallel()
+                // enforces sorted results
+                .AsOrdered()
+                // enforces parallelism
+                .WithExecutionMode(ParallelExecutionMode.ForceParallelism)
+                // sets max cores
+                .WithDegreeOfParallelism(8)
+                .Where(i => i % 2 == 0)
+                .ToArray();
+
+            foreach (int i in parallelResults)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
