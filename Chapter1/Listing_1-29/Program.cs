482f476afcaab68ee79e5839c438c5899156d313
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-08 10:51:24 +0100

    Consuming results from BlockingCollection in read task

---
+++ Chapter1/Listing_1-29/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlockingCollection<string> collection = new BlockingCollection<string>();
+
+            Task readTask = Task.Run(() =>
+            {
+                foreach (string input in collection.GetConsumingEnumerable())
+                {
+                    Console.WriteLine($"You've entered: \"{input}\"");
+                    Console.Write("enter text: ");
+                }
+            });
+
+            Task writeTask = Task.Run(() =>
+            {
+                Console.Write("enter text: ");
+                while (true)
+                {
+                    string input = Console.ReadLine();
+                    if (string.IsNullOrWhiteSpace(input)) break;
+                    collection.Add(input);
+                }
+            });
+
+            writeTask.Wait();
+        }
+    }
+}
