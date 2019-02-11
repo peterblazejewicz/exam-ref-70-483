8cc4a5666896db412adb10f943965d808ada1d1c
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-11 22:29:49 +0100

    Using lock to access shared data

---
+++ Chapter1/Listing_1-36/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            object _lock = new { };
+
+            Task task = Task.Run(() =>
+            {
+                for (int i = 0; i < 1_000_000; i++)
+                {
+                    lock (_lock)
+                    {
+                        n++;
+                    }
+                }
+            });
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                lock (_lock)
+                {
+                    n--;
+                }
+            }
+
+            task.Wait();
+            Console.WriteLine(n);
+        }
+    }
+}
