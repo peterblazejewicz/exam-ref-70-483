fa2a5bb594cec7df1f4decff5807bdbfd458e1aa
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-11 22:59:39 +0100

    Lock deadlock example

---
+++ Chapter1/Listing_1-37/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1.Listing_1_37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            object lockA = new object();
+            object lockB = new object();
+
+            Task task = Task.Run(() =>
+            {
+                Console.WriteLine("Lock A");
+                lock (lockA)
+                {
+                    Console.WriteLine($"Sleep {1_000}");
+                    Thread.Sleep(1_000);
+                    lock (lockB)
+                    {
+                        Console.WriteLine("Lock B");
+                    }
+                }
+            });
+
+            Console.WriteLine("Lock B");
+            lock (lockB)
+            {
+                Console.WriteLine($"Sleep {1_000}");
+                Thread.Sleep(1_000);
+                lock (lockA)
+                {
+                    Console.WriteLine("Lock A");
+                }
+            }
+
+            task.Wait();
+
+        }
+    }
+}
