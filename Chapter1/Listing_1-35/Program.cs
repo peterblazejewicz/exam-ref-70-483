915c94017436c58a813cb05b66b76d1968bf7271
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-11 22:19:47 +0100

    Shared data access listing

---
+++ Chapter1/Listing_1-35/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Chapter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Task task = Task.Run(() =>
+            {
+                for (int i = 0; i < 1_000_000; i++)
+                {
+                    n++;
+                }
+            });
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                n--;
+            }
+
+            task.Wait();
+            Console.WriteLine(n);
+        }
+    }
+}
