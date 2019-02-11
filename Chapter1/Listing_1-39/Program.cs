035297618b6512bb8830d6cc0c0bca69db01ffb4
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-11 23:21:13 +0100

    Using volatile keyword with threads

---
+++ Chapter1/Listing_1-39/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Chapter1.Listing_1_39
+{
+    class Program
+    {
+
+        static volatile int _flag = 0;
+        static int _value = 0;
+
+        static void Thread1()
+        {
+            _value = 5;
+            _flag = 1;
+        }
+
+        static void Thread2()
+        {
+            if (_flag == 1)
+            {
+                Console.WriteLine(_value);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(Thread1);
+            Thread thread2 = new Thread(Thread2);
+            thread1.Start();
+            thread2.Start();
+        }
+    }
+}
