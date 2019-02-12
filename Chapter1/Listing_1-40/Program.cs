34845980ed54d0963599b8f4e8523b9cc14ae665
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-12 22:26:06 +0100

    Using Interlocked class to manage access

---
+++ Chapter1/Listing_1-40/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1.Listing_1_40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Task task = Task.Run(() =>
+            {
+                for (int i = 0; i < 1_000_000; i++)
+                {
+                    Interlocked.Increment(ref n);
+                }
+            });
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+                Interlocked.Decrement(ref n);
+            }
+
+            task.Wait();
+            Console.WriteLine(n);
+        }
+    }
+}
