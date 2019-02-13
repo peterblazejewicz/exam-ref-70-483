d8939a257d89527937d83bd7cc6bf2d4da36d077
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-13 23:50:45 +0100

    Comparing and changing values in non-atomic way

---
+++ Chapter1/Listing_1-41/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chatper1.Listing_1_41
+{
+    class Program
+    {
+
+        static int value = 1;
+        static void Main(string[] args)
+        {
+            Task task1 = Task.Run(() =>
+            {
+                if (value == 1)
+                {
+                    // Removing the following line will change the output
+                    Thread.Sleep(1_000);
+                    value = 2;
+                }
+            });
+
+            Task task2 = Task.Run(() =>
+            {
+                Thread.Sleep(500);
+                value = 3;
+            });
+
+            Task.WaitAll(task1, task2);
+            Console.WriteLine(value);
+
+        }
+    }
+}
