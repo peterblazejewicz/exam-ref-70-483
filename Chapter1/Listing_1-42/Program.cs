65d359bddb5a3bf168902c42e371af69ce11f6b6
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-14 23:27:07 +0100

    Using cancellation token to cancel task

---
+++ Chapter1/Listing_1-42/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chapter1.Listing_1_42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+            Task task = Task.Run(() =>
+            {
+                while (!cancellationToken.IsCancellationRequested)
+                {
+                    Console.WriteLine("*");
+                    Thread.Sleep(1_000);
+                }
+            }, cancellationToken);
+
+            Console.WriteLine("Press Enter to stop task");
+            Console.ReadLine();
+            cancellationTokenSource.Cancel();
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
