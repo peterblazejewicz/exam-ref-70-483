cc9a288bf1836cef31d000bb0e4365d7bcc4eb38
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2019-02-16 00:57:19 +0100

    Timing out long running task

---
+++ Chapter1/Listing_1-45/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Chatper1.Listing_1_45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task = Task.Run(() =>
+            {
+                Thread.Sleep(10_000);
+            });
+
+            int status = Task.WaitAny(new[] { task }, 1_000);
+
+            if (status == -1)
+            {
+                Console.WriteLine("Task timed out");
+            }
+        }
+    }
+}
